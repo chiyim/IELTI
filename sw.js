30875b88060c4c35f84149a2dee8fcf22d45a612
chiyim <chilamc.y@gmail.com>
2026-07-29 11:53:10 +0800

    nav

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE='ielti-shell-v116';
+const CACHE='ielti-shell-v117';
 const SHELL=['./','./index.html','./apple-ui.css','./eink-ui.css','./ielti-config.js','./ielti-core.js','./ielts-roadmap-data.js','./manifest.webmanifest','./icon.png','./icon_local.png','./ielts-roadmap.html','./ielts-core-vocabulary.html','./ielts-vocabulary-categories.html','./ielts_word_memory_v2_ipa.html','./121-letter-combinations.html'];
 self.addEventListener('install',event=>event.waitUntil(caches.open(CACHE).then(cache=>cache.addAll(SHELL)).then(()=>self.skipWaiting())));
 self.addEventListener('activate',event=>event.waitUntil(caches.keys().then(keys=>Promise.all(keys.filter(key=>key!==CACHE).map(key=>caches.delete(key)))).then(()=>self.clients.claim())));
